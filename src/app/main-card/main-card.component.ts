3c8d81ca935d29c7f13c4847be3739465295b7e4
Hugo Martin <hugomartin.menz@gmail.com>
2023-01-12 12:04:21 +0100

    Updated text parser. Implemented data preparation for mat-table

--- src/app/main-card/main-card.component.ts
+++ src/app/main-card/main-card.component.ts
@@ -1,43 +1,54 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-
-import { Subject, Observable, of } from 'rxjs';
-import { map, catchError, tap } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
 import { Papa } from 'ngx-papaparse';
+import { MatTableDataSource } from '@angular/material/table';
+import { Item } from '../core/interfaces/item';
 
 @Component({
   selector: 'app-main-card',
   templateUrl: './main-card.component.html',
   styleUrls: ['./main-card.component.scss'],
 })
-export class MainCardComponent implements OnInit, OnDestroy {
-  dragDropSubject: Subject<File[]> = new Subject<File[]>();
+export class MainCardComponent implements OnInit {
+  data: Item[] = [] as Item[];
 
+  dataSource: MatTableDataSource<Item> = new MatTableDataSource<Item>([]);
+  dataEmpty: boolean = true;
+
+  displayedColumns = ['id', 'code', 'desc1', 'desc2', 'dimension', 'quantity'];
   constructor(private papa: Papa) {}
 
-  // init
-  ngOnInit() {
-    this.dragDropSubject.subscribe((data) => {
-      console.log(data);
-    });
-  }
+  ngOnInit() {}
 
   onFileDropped(event: any) {
     const files = event.target.files[0];
     const reader = new FileReader();
-
     reader.onload = () => {
       this.papa.parse(reader.result as string, {
         complete: (result) => {
-          result.data.map((element: any) => console.log(element));
+          this.data = [];
+          result.data.slice(1, -1).map((element: string[]) =>
+            this.data.push({
+              id: element[0],
+              code: element[1],
+              desc1: element[2],
+              desc2: element[3],
+              dimension: element[4],
+              quantity: element[5],
+            })
+          );
+          console.log(this.data);
+          this.dataEmpty = false;
+
+          this.dataSource = new MatTableDataSource<Item>(this.data);
         },
       });
     };
-
     reader.readAsText(files);
   }
 
-  onButtonPaste() {}
+  parseText(event: any) {
+    this.onFileDropped(event);
+  }
 
-  // destroy
-  ngOnDestroy() {}
+  onButtonPaste() {}
 }
