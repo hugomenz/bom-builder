752de49cb11cca05c4e12d281fdedd8faefda834
Hugo Martin <hugomartin.menz@gmail.com>
2023-01-25 21:37:38 +0100

    Parser outside of component. Type and header check

---
+++ src/app/core/services/csv.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { Papa } from 'ngx-papaparse';
+import { from, map, Observable } from 'rxjs';
+import { Item } from '../interfaces/item';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CsvService {
+  data: Item[] = [] as Item[];
+  dataEmpty: boolean = true;
+
+  constructor(private papa: Papa) {}
+
+  checkFileType(file: any): boolean {
+    return file.type === 'text/csv';
+  }
+
+  checkFileHeader(data: any): boolean {
+    return Object.keys(data[0]).length === 6;
+  }
+
+  csvParser(files: any): Promise<Item[]> {
+    // onload needs a little bit to load the data it has to be async
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      if (!this.checkFileType(files)) {
+        reject(new Error('Invalid file type'));
+        return;
+      }
+
+      reader.onload = () => {
+        this.papa.parse(reader.result as string, {
+          complete: (result) => {
+            if (!this.checkFileHeader(result.data)) {
+              reject(new Error('Invalid file header'));
+              return;
+            }
+
+            this.data = [];
+
+            // skip position 0 for headers
+            result.data.slice(1, -1).map((element: string[]) => {
+              this.data.push({
+                id: element[0],
+                code: element[1],
+                desc1: element[2],
+                desc2: element[3],
+                dimension: element[4],
+                quantity: element[5],
+              });
+            });
+            this.dataEmpty = false;
+            resolve(this.data);
+          },
+        });
+      };
+      reader.readAsText(files);
+    });
+  }
+}
--- src/app/main-card/main-card.component.ts
+++ src/app/main-card/main-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Papa } from 'ngx-papaparse';
 import { MatTableDataSource } from '@angular/material/table';
 import { Item } from '../core/interfaces/item';
+import { CsvService } from '../core/services/csv.service';
 
 @Component({
   selector: 'app-main-card',
@@ -9,45 +10,21 @@ import { Item } from '../core/interfaces/item';
   styleUrls: ['./main-card.component.scss'],
 })
 export class MainCardComponent implements OnInit {
-  data: Item[] = [] as Item[];
-
+  testdata: any;
   dataSource: MatTableDataSource<Item> = new MatTableDataSource<Item>([]);
-  dataEmpty: boolean = true;
-
   displayedColumns = ['id', 'code', 'desc1', 'desc2', 'dimension', 'quantity'];
-  constructor(private papa: Papa) {}
+  constructor(public csv: CsvService) {}
 
   ngOnInit() {}
 
   onFileDropped(event: any) {
     const files = event.target.files[0];
-    const reader = new FileReader();
-    reader.onload = () => {
-      this.papa.parse(reader.result as string, {
-        complete: (result) => {
-          this.data = [];
-          result.data.slice(1, -1).map((element: string[]) =>
-            this.data.push({
-              id: element[0],
-              code: element[1],
-              desc1: element[2],
-              desc2: element[3],
-              dimension: element[4],
-              quantity: element[5],
-            })
-          );
-          console.log(this.data);
-          this.dataEmpty = false;
 
-          this.dataSource = new MatTableDataSource<Item>(this.data);
-        },
-      });
-    };
-    reader.readAsText(files);
-  }
-
-  parseText(event: any) {
-    this.onFileDropped(event);
+    this.csv
+      .csvParser(files)
+      .then(
+        (result) => (this.dataSource = new MatTableDataSource<Item>(result))
+      );
   }
 
   onButtonPaste() {}
