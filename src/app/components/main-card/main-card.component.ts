69b412f137f85859ce0e687029afb225de7de740
Hugo Martin <hugomartin.menz@gmail.com>
2023-01-29 09:13:13 +0100

    Mocked file

--- src/app/components/main-card/main-card.component.ts
+++ src/app/components/main-card/main-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from 'src/app/core/services/localstorage.service';
 import { CsvService } from '../../core/services/csv.service';
 
 @Component({
@@ -6,12 +7,32 @@ import { CsvService } from '../../core/services/csv.service';
   templateUrl: './main-card.component.html',
   styleUrls: ['./main-card.component.scss'],
 })
-export class MainCardComponent {
+export class MainCardComponent implements OnInit {
   spinner = false;
 
   droppedFile!: File | boolean; // boolean for test. It should be removed when "new List" is implemented
+  // +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // mocked file variables
+  mockedFileData: any;
+  //
+  // +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  constructor(
+    public csv: CsvService,
+    private _storage: LocalStorageService // mocking file service
+  ) {}
 
-  constructor(public csv: CsvService) {}
+  // +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  // - delete implements OnInit
+  // - delete input directive html component
+  // mocking file block
+  ngOnInit() {
+    let dataFromStorage = this._storage.getObject('droppedFile');
+    if (Object.keys(dataFromStorage).length > 0) {
+      this.mockedFileData = dataFromStorage;
+    }
+  }
+  //
+  // +++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
   onFileDropped(event: any) {
     this.spinner = true;
